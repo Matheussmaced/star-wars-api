8d9d006be2545b79a8166a1fee7338abb2765940
Matheussmaced <mateusbyakuya3@gmail.com>
2025-01-09 20:21:52 -0300

    feat: config db

---
+++ src/environments/environments.js
@@ -0,0 +1,3 @@
+export const environment = {
+  DB_URL : 'mongodb+srv://admin:admin@starwars-api.wcw8f.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api'
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-const express = require("express")
+
+import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
+app.use(express.json());
 const port = 3000;
+mongoose.connect('mongodb+srv://admin:admin@starwars-api.wcw8f.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api');
+
+const Film = mongoose.model('Film', {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.post('/', async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url
+  })
+
+  await film.save()
+  res.send(film)
 })
 
 app.listen(port, () => {
-  console.log(`A aplicação está rodando na porta ${port}`)
-})
+  console.log(`A aplicação está rodando na porta ${port}`);
+});
