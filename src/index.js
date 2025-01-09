a0893f81fefb05f40a3e96a89a21da1948d139a2
Matheussmaced <mateusbyakuya3@gmail.com>
2025-01-09 20:37:30 -0300

    feat: creating method put

--- src/index.js
+++ src/index.js
@@ -31,6 +31,19 @@ app.post('/', async (req, res) => {
   return res.send(film)
 })
 
+app.put('/:id', async(req, res) => {
+  const film = await Film.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url
+  }, {
+    new: true
+  })
+
+  return res.send(film)
+})
+
 app.delete('/:id', async(req, res) => {
   const film = await Film.findByIdAndDelete(req.params.id)
   return res.send(film)
